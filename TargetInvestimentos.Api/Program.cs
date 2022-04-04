79035cfbda6649ffd9a2fde3bbcb12bae59bf637
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 03:47:02 -0300

    Faz conexão com banco de dado SQL #19

--- TargetInvestimentos.Api/Program.cs
+++ TargetInvestimentos.Api/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TargetInvestimentos.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+var DATABASE_HOST = Environment.GetEnvironmentVariable("DATABASE_HOST");
+var DATABASE_PASSWORD = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer($"Server={DATABASE_HOST};Database=app;Trusted_Connection=True;Password={DATABASE_PASSWORD}");
+});
 
 var app = builder.Build();
 
