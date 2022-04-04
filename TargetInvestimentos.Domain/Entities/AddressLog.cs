c75c371371e478fc99ac24780f441e56893e48fd
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 03:02:59 -0300

    Adiciona entidades para os Logs #19

---
+++ TargetInvestimentos.Domain/Entities/AddressLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetInvestimentos.Domain.Entities
+{
+    public class AddressLog
+    {
+
+        [Key]
+        public int ID_AddressLog { get; set; }
+        public int ID_Address { get; set; }
+
+        [StringLength(1)]
+        public string CD_TypeLog { get; set; }
+
+        [StringLength(8)]
+        public string CD_PostalCode { get; set; }
+
+        [StringLength(100)]
+        public string NM_State { get; set; }
+
+        [StringLength(2)]
+        public string CD_StateAcronym { get; set; }
+
+        [StringLength(200)]
+        public string DS_PublicPlace { get; set; }
+
+        [StringLength(200)]
+        public string NM_City { get; set; }
+
+        [StringLength(200)]
+        public string DS_Complement { get; set; }
+
+        [StringLength(200)]
+        public string NM_District { get; set; }
+
+    }
+}
---
+++ TargetInvestimentos.Domain/Entities/PersonLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetInvestimentos.Domain.Entities
+{
+    public class PersonLog
+    {
+        [Key]
+        public int ID_PersonLog { get; set; }
+        public int ID_Person { get; set; }
+
+        [StringLength(1)]
+        public string CD_TypeLog { get; set; }
+
+        public int ID_Address { get; set; }
+
+        public DateTime DT_BirthDate { get; set; }
+
+        public DateTime DT_RegisterDate { get; set; }
+
+        [StringLength(200)]
+        public string NM_FullName { get; set; }
+
+        public bool IS_Vip { get; set; }
+
+        public decimal MonthlyIncome { get; set; }
+
+        [StringLength(8)]
+        public string Cpf { get; set; }
+    }
+}
