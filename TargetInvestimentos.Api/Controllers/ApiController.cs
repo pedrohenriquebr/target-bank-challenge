3fa0bd95ff32d8893a072a318110e87bd1899633
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 09:52:08 -0300

    Adiciona Controller #19

---
+++ TargetInvestimentos.Api/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TargetInvestimentos.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TargetInvestimentos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvestimentosController : ControllerBase
+    {
+
+        private Context _context;
+
+        public InvestimentosController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStatus()
+        {
+            return Ok("ok");
+        }
+
+    }
+}
