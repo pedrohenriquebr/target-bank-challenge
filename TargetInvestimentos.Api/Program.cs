aa8ff58246dcf2b063aeb5c5f7e2b11608b6d772
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 10:30:30 -0300

    Adicion opção ao carregar JSON para evitar loops infinitos #19

--- TargetInvestimentos.Api/Program.cs
+++ TargetInvestimentos.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using TargetInvestimentos.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,13 +7,16 @@ var DATABASE_HOST = Environment.GetEnvironmentVariable("DATABASE_HOST");
 var DATABASE_PASSWORD = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Context>(options =>
 {
-    options.UseSqlServer($"Server={DATABASE_HOST};Database=app;user id=sa;Password={DATABASE_PASSWORD};TrustServerCertificate=true");
+    options
+    .UseLazyLoadingProxies()
+    .UseSqlServer($"Server={DATABASE_HOST};Database=app;user id=sa;Password={DATABASE_PASSWORD};TrustServerCertificate=true");
 });
 
 var app = builder.Build();
