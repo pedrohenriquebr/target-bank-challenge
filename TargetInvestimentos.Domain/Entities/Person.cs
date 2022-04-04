89bcc2a6de3b79b3b10d1d6ee143e1bc834b5446
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 02:58:21 -0300

    Adiciona entidade Person #19

---
+++ TargetInvestimentos.Domain/Entities/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetInvestimentos.Domain.Entities
+{
+    public class Person
+    {
+		[Key]
+		public int ID_Person {get;set;}
+		
+		public int ID_Address  {get;set;}
+		
+		public DateTime DT_BirthDate {get;set;}
+		
+		public DateTime DT_RegisterDate {get;set;}
+        
+		[StringLength(200)]
+		public string NM_FullName {get;set;}
+		
+		public bool IS_Vip {get;set;}
+		
+		public decimal MonthlyIncome {get;set;}
+
+        [StringLength(8)]
+		public string Cpf {get;set;}
+
+		public virtual Address address { get; set; }
+	}
+}
