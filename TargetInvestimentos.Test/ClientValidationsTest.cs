30abe57a2eba1d2412f609a2ec40f357ef255b39
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 08:29:38 -0300

    Adiciona teste unitário para validação de CPF #17

---
+++ TargetInvestimentos.Test/ClientValidationsTest.cs
@@ -0,0 +1,21 @@
+using TargetInvestimentos.Domain.Validations;
+using Xunit;
+
+namespace TargetInvestimentos.Test
+{
+    public class ClientValidationsTest
+    {
+        [Fact]
+        public void TestValidateCPF()
+        {
+            Assert.False(ClientValidations.ValidateCPF("123.123.123-99"));
+            Assert.False(ClientValidations.ValidateCPF("123.123.123-"));
+            Assert.False(ClientValidations.ValidateCPF("123.123.  -"));
+            Assert.False(ClientValidations.ValidateCPF("123.undefined.  -"));
+            Assert.False(ClientValidations.ValidateCPF("###.undefined.  -"));
+            Assert.False(ClientValidations.ValidateCPF(".undefined.  -"));
+            Assert.False(ClientValidations.ValidateCPF("123.123.12399"));
+            Assert.False(ClientValidations.ValidateCPF("vv"));
+        }
+    }
+}
