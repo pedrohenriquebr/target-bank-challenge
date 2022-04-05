21e066e6fe0f72422809693d9d63ecff46d2030d
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 09:34:40 -0300

    Cria service para consumir api do IBGE #18

---
+++ TargetInvestimentos.Api/Services/IBGEService.cs
@@ -0,0 +1,18 @@
+﻿using TargetInvestimentos.Domain.Entities;
+using TargetInvestimentos.Domain.Interfaces;
+
+namespace TargetInvestimentos.Api.Services
+{
+    public class IBGEService : IIBGEService
+    {
+        public Task<ICollection<City>> GetAllCities(string uf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<State>> GetAllStates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
