17c69f39afa0ba24aa47cd95868070cb2094d226
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 00:04:21 -0300

    Adiciona service e interface para Client #15

---
+++ TargetInvestimentos.Api/Services/ClientService.cs
@@ -0,0 +1,16 @@
+﻿using TargetInvestimentos.Infrastructure;
+
+namespace TargetInvestimentos.Api.Services
+{
+    public class ClientService
+    {
+
+        private readonly Context _context;
+        public ClientService(Context context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
---
+++ TargetInvestimentos.Domain/Interfaces/IClientService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TargetInvestimentos.Domain.Entities;
+
+namespace TargetInvestimentos.Domain.Interfaces
+{
+    public interface IClientService
+    {
+
+    }
+}
