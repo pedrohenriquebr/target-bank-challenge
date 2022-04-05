7c05ff0e2fd6e1eaf844375e7b1173932073142a
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 09:25:22 -0300

    Interface serviço do IBGE #18

---
+++ TargetInvestimentos.Domain/Interfaces/IIBGEService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TargetInvestimentos.Domain.Entities;
+
+namespace TargetInvestimentos.Domain.Interfaces
+{
+    public interface IIBGEService
+    {
+
+        public Task<ICollection<State>> GetAllStates();
+        public Task<ICollection<City>> GetAllCities(string uf);
+    }
+}
