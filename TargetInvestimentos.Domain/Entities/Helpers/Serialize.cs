4e7e58d595433548a9423c497eb22c0effda6ba1
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 09:32:38 -0300

    Cria helper para entidades #18

---
+++ TargetInvestimentos.Domain/Entities/Helpers/Serialize.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TargetInvestimentos.Api.Helpers;
+
+namespace TargetInvestimentos.Domain.Entities.Helpers
+{
+    public static class Serialize
+    {
+        public static string ToJson(this Object self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+}
