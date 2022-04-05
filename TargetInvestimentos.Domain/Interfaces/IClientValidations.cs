1025c4ecaee88b1aa34aac4891d9fbfaa44030e7
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 00:20:15 -0300

    Interface para validações de regra de negócio do dados do cliente #15 #17

---
+++ TargetInvestimentos.Domain/Interfaces/IClientValidations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetInvestimentos.Domain.Interfaces
+{
+    public interface IClientValidations
+    {
+        public static bool ValidateCPF(string cpf) => throw new NotImplementedException();
+        public static bool ValidateCEP(string cep) =>  throw new NotImplementedException();
+    }
+}
