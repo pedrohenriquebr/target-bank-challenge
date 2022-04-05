7c0ae06a68196ce061d1fb81e81f15323fbe6325
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 22:02:54 -0300

    cria método para adapter para converter Modelo em Entidade #15

--- TargetInvestimentos.Api/Adapters/InvestimentosAdapter.cs
+++ TargetInvestimentos.Api/Adapters/InvestimentosAdapter.cs
@@ -1,6 +1,30 @@
-﻿namespace TargetInvestimentos.Api.Adapters
+﻿using TargetInvestimentos.Api.Models;
+using TargetInvestimentos.Domain.Entities;
+
+namespace TargetInvestimentos.Api.Adapters
 {
     public class InvestimentosAdapter
     {
+
+        public static Person RegisterModelToEntity(RegisterClientCommand command)
+        {
+            return new Person
+            {
+                NM_FullName = command.nomeCompleto,
+                DT_BirthDate = command.dataNascimento,
+                Cpf = command.cpf,
+                MonthlyIncome = command.rendaMensal,
+                Address = new Address
+                {
+                    CD_PostalCode = command.cep,
+                    CD_StateAcronym = command.uf,
+                    NM_State = command.estado,
+                    DS_Complement = command.complemento,
+                    DS_PublicPlace = command.logradouro,
+                    NM_City = command.cidade,
+                    NM_District = command.bairro
+                }
+            };
+        }
     }
 }
