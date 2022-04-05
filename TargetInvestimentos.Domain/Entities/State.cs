8f58c547d6f631ec5ff79be47ec1eec1b2c53e3e
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-05 09:25:36 -0300

    Cria entidade/modelo para Estado #18

---
+++ TargetInvestimentos.Domain/Entities/State.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TargetInvestimentos.Api.Helpers;
+
+namespace TargetInvestimentos.Domain.Entities
+{
+    public partial class State
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("sigla")]
+        public string Sigla { get; set; }
+
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+
+    }
+    public partial class State
+    {
+        public static State FromJson(string json) => JsonConvert.DeserializeObject<State>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this State self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    
+}
