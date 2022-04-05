8d874a1997462666273fac2fb397dc497c1cac15
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-04 21:45:15 -0300

    Adiciona classe de modelo para cadastro de Novos Cliente  #15

---
+++ TargetInvestimentos.Api/Models/RegisterClientCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TargetInvestimentos.Api.Models
+{
+    public class RegisterClientCommand
+    {
+        [Required]
+        public string fullName { get; set; }
+
+        [Required]
+        public DateTime birthDate { get; set; }
+
+        [Required]
+        public string cpf { get; set; }
+    }
+}
