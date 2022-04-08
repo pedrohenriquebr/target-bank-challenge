7f25f4d8cac21f3042ddd8a51937209950053084
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-08 09:48:45 -0300

    Cria modelo para retornar status do cadastro do cliente #16

---
+++ TargetInvestimentos.Api/Models/RegisterClientResult.cs
@@ -0,0 +1,8 @@
+﻿namespace TargetInvestimentos.Api.Models
+{
+    public class RegisterClientResult
+    {
+        public bool Cadastrado { get; set; }
+        public bool OferecerPlanoVip { get; set; }
+    }
+}
