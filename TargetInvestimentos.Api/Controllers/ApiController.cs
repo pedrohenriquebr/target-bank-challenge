d73cf34177fda8aa0a347ac874ec3720b5337cf5
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-08 09:49:23 -0300

    Rota para cadastro de cliente #16 #15

--- TargetInvestimentos.Api/Controllers/ApiController.cs
+++ TargetInvestimentos.Api/Controllers/ApiController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TargetInvestimentos.Api.Adapters;
 using TargetInvestimentos.Api.Models;
+using TargetInvestimentos.Api.Services;
 using TargetInvestimentos.Domain.Entities;
+using TargetInvestimentos.Domain.Interfaces;
+using TargetInvestimentos.Domain.Validations;
 using TargetInvestimentos.Infrastructure;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,18 +17,36 @@ namespace TargetInvestimentos.Api.Controllers
     {
 
         private Context _context;
+        private readonly IClientService _service;
 
         public InvestimentosController(Context context)
         {
             _context = context;
+            _service = new ClientService(context);
         }
 
         [HttpPost("client")]
         public async Task<IActionResult> PostClient([FromBody] RegisterClientCommand command)
         {
+            try
+            {
+                var entity = InvestimentosAdapter.RegisterModelToEntity(command);
+                await _service.RegisterClient(entity);
 
+                return Ok(new RegisterClientResult
+                {
+                    Cadastrado = true,
+                    OferecerPlanoVip = ClientValidations.ValidateIsVip(entity)
+                });
 
-            return Ok("ok");
+            }catch(Exception e)
+            {
+                return BadRequest(new
+                {
+                    Message = e.Message,
+                    Cadastrado = false
+                });
+            }
         }
 
     }
